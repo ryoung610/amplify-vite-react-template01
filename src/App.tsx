237dc0419ad026f29d398e939c0a601b40a2b695
ryoung610 <me0610@gmail.com>
2024-10-09 17:38:35 -0400

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-import { useAuthenticator } from '@aws-amplify/ui-react';
-
+import { Authenticator } from '@aws-amplify/ui-react';
 
 const client = generateClient<Schema>();
 
@@ -10,38 +9,48 @@ function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
+    const subscription = client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
+
+    return () => subscription.unsubscribe(); // Cleanup the subscription on unmount
   }, []);
-  const { signOut } = useAuthenticator();
-  
+
   function deleteTodo(id: string) {
     client.models.Todo.delete({ id })
+      .catch(error => console.error("Error deleting todo:", error));
   }
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    const content = window.prompt("Todo content");
+    if (content) {
+      client.models.Todo.create({ content })
+        .catch(error => console.error("Error creating todo:", error));
+    }
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 successfully hosted webscraper app a new todo.
-        <br />
-        <a href="https://bobbydigi1.com/">
-          webscraper built by LL&L
-        </a>
-      </div>
-      <button onClick={signOut}>Sign out</button>
-    </main>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>{user?.signInDetails?.loginId}'s todos</h1>
+          <button onClick={createTodo}>+ new</button>
+          <ul>
+            {todos.map((todo) => (
+              <li onClick={() => deleteTodo(todo.id)} key={todo.id}>
+                {todo.content}
+              </li>
+            ))}
+          </ul>
+          <div>
+            🥳 successfully hosted webscraper app a new todo.
+            <br />
+            <a href="https://bobbydigi1.com/">webscraper built by LL&L</a>
+          </div>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
